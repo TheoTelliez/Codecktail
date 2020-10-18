08151e20125ef17b8f05940ff1e3b4c241d120c7
TheoTelliez <theo.telliez@epsi.fr>
2020-10-18 11:36:19 +0200

    Dimanche matin listes

--- app/src/main/java/com/example/appcourstrois/ListFragment.java
+++ app/src/main/java/com/example/appcourstrois/ListFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -16,4 +15,4 @@ public class ListFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_list, null); //le 2e paramètre sera toujours null tandis que le premier représentera l’id de notre layout de fragment (ici : R.layout.fragment_random).
     }
-}
+    }
