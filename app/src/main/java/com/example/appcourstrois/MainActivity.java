7233d76347031b9d5bf288ca738399a932b0cdc9
TheoTelliez <theo.telliez@epsi.fr>
2020-11-02 17:06:54 +0100

    Après supp liste

--- app/src/main/java/com/example/appcourstrois/MainActivity.java
+++ app/src/main/java/com/example/appcourstrois/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.appcourstrois;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+
 import android.os.Bundle;
 import android.view.MenuItem;
 
